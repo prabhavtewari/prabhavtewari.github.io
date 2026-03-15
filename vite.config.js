c427420946a7ce70b545151db3aaa6062c770911
Prabhav <prabhavtewari@gmail.com>
2026-03-15 01:53:18 +0100

    IVAR blog

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: '/index.html',
-        blog: '/IARVR_Blog.html'
+        blog: '/IVAR/index.html',
+        motivation: '/IVAR/motivation.html',
+        map: '/IVAR/map.html',
+        cue: '/IVAR/cue.html',
+        playthrough: '/IVAR/playthrough.html',
+        closing: '/IVAR/closing.html',
+        interaction: '/IVAR/interaction.html',
+        locomotion: '/IVAR/locomotion.html'
       }
     }
   }
